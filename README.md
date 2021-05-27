20fa2bbf194e37dba72bb3823f1c0e89b590fc7e
combi_jihoon <combi_jihoon@naver.com>
2021-05-27 14:13:20 +0900

    Put secret_key into json and reinitializ remote repository

---
+++ README.md
@@ -0,0 +1,6 @@
+# 코드잇 Django 강의
+
+### [나의 강의 목적]  
+1) Django의 특징을 이해한다.  
+2) Django의 구조를 이해한다.  
+3) Django를 이용해 real-classifier 웹 페이지를 만든다!
---
+++ foods/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.http import Http404
+
+# Create your views here.
+
+
+def index(request):
+    today = datetime.today().date()
+    context = {"date": today}
+    return render(request, 'foods/index.html', context=context)
+
+
+def food_detail(request, food):
+    context = dict()
+    if food == 'chicken':
+        context["name"] = "코딩에 빠진 닭"
+        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격~!"
+        context["price"] = 1000
+        context["img_path"] = "foods/images/chicken.jpg"  # 정적 파일 경로
+    else:
+        raise Http404("이런 음식은 없어욤^_^;;;;;;;")
+    return render(request, 'foods/detail.html', context=context)
